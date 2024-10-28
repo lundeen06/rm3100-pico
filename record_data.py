f028484dc9fdb34a90b89fb4932100fb40fcb7ef
lundeen06 <lundeen.cahilly@gmail.com>
2024-10-27 22:27:03 -0700

    serial addr

--- record_data.py
+++ record_data.py
@@ -1,7 +1,7 @@
 import serial
 import time
 
-with serial.Serial('/dev/tty.usbmodem2101', 115200) as ser:
+with serial.Serial('/dev/tty.usbmodem1101', 115200) as ser:
     with open('./output/magnetometer_data.csv', 'w') as f:
         while True:
             try:
